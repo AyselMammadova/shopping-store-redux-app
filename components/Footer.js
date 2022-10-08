9f67af6607a93451b4cda6987d08ed116fd351f0
AyselMammadova <ayselmemmedova9317@gmail.com>
2022-10-09 01:07:42 +0400

    header, footer, categories with responsive design

---
+++ components/Footer.js
@@ -0,0 +1,201 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-white">
+        <div className="section-top py-[30px] bg-bl">
+            <div className="container mx-auto max-w-full">
+                <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-6 gap-16">
+                    <div className="foot-list">
+                        <h6 className="text-lg font-semibold tracking-wider">
+                            Biz kimiz?
+                        </h6>
+
+                        <ul>
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Hakkımızda 
+                                </a>
+                            </li> 
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Kariyer
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    İletişim 
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Bizde Satış Yap 
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="foot-list">
+                        <h6 className="text-lg font-semibold tracking-wider">
+                            Yardım
+                        </h6>
+                        
+                        <ul>
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Sıkça Sorulan Sorular 
+                                </a>
+                            </li> 
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Canlı Yardım 
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Naslk İade Ederim  
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Nasıl Sipariş Verilir 
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="foot-list">
+                        <h6 className="text-lg font-semibold tracking-wider">
+                            Kampanyalar
+                        </h6>
+                        
+                        <ul>
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Özel kampanyalar 
+                                </a>
+                            </li> 
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Bayram kampanyaları
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    Aktif Kampanyalar 
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline">
+                                    VIP Üyelik
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="foot-list">
+                        <h6 className="text-lg font-semibold tracking-wider">
+                            Sosyal Medya
+                        </h6>
+                        
+                        <ul>
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline flex items-center">
+                                    <img src="/images/facebook.svg" alt="facebook" className="mr-[15px]" />
+                                    Facebook 
+                                </a>
+                            </li> 
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline flex items-center">
+                                    <img src="/images/instagram.svg" alt="instagram" className="mr-[15px]" />
+                                    Instagram
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline flex items-center">
+                                    <img src="/images/twitter.svg" alt="twitter" className="mr-[15px]" />
+                                    Twitter 
+                                </a>
+                            </li>
+
+                            <li className="mt-[20px]">
+                                <a href="#" className="text-sm hover:underline flex items-center">
+                                    <img src="/images/youtube.svg" alt="youtube" className="mr-[15px]" />
+                                    Youtube
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="foot-list col-span-2">
+                        <h6 className="text-lg font-semibold tracking-wider">
+                            İndirim ve yenilikler için abone olun
+                        </h6>
+                        
+                        <form role="form" className="mt-[20px] flex items-center">
+                            <label className="block">
+                                <span className="sr-only">E-mail</span>
+                                <input 
+                                    className="placeholder:text-grey1 placeholder:text-base bg-light block 
+                                    text-base w-full border-solid border border-grey2 rounded-[4px]
+                                    sm:w-[256px] h-[50px] px-[16px] focus:outline-none" 
+                                    placeholder="E-mail" type="text" name="e-mail" id="e-mail" 
+                                />
+                            </label>
+                            <button className="h-[50px] rounded-[4px] bg-red text-white shadow-shadow1 
+                            px-[16px] text-base font-semibold ml-[9px]">
+                                Abone ol
+                            </button>
+                        </form>
+
+                        <div className="note text-sm mt-[20px] w-[60%]">
+                            Bültene kaydolarak Gizlilik Politikası
+                            <a href="#" className="underline mr-[4px] inline-block">Gizlilik Politikasını</a> 
+                            kabul etmiş olursunuz
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="section-bottom py-[14px] bg-bl2">
+            <div className="container mx-auto max-w-full">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-16 gap-y-4 items-center">
+                    <p className="text-xsm mb-0 col-span-1 lg:col-span-2 xl:col-span-1">
+                        © Telif Hakkı 2022 -  testwebsite.com 
+                    </p>
+
+                    <a href="#" className="text-xsm">
+                        Kullanım Koşulları
+                    </a>
+
+                    <a href="#" className="text-xsm">
+                        KVK ve Gizlilik Politikası
+                    </a>
+
+                    <div className="foot-list xl:col-span-3">
+                        <div className="flex items-center lg:justify-end">
+                            <img src="/images/mastercard.svg" alt="mastercard" className="lg:ml-[40px]" />
+                            <img src="/images/visa.svg" alt="visa" className="ml-[10px] sm:ml-[40px]" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,122 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Source Sans Pro', 'sans-serif'],
+      body: ['Source Sans Pro', 'sans-serif']
+    },
+    fontSize: {
+      xsm: ['12px', '15px'],
+      sm: ['14px', '18px'],
+      base: ['16px', '20px'],
+      lg: ['18px', '23px'],
+      xl: ['24px', '32px'],
+    },
+    colors: {
+      white: '#fff',
+      red: '#FF0000',
+      lred: '#FAF3F3',
+      grey: '#D5D5D5',
+      grey1: 'rgba(0, 0, 0, 0.4)',
+      grey2: 'rgba(196, 196, 196, 0.3)',
+      grey3: '#FFFFFF1A',
+      light: '#F8F8F8',
+      bl: '#26282B',
+      bl2: '#221F23',
+      lwhite: 'rgba(255, 255, 255, 0.1)',
+      lwhite2: '#FEFEFE'
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '207px',
+      },
+    },
+    screens: {
+      'ssm': '500px',
+      'sm': '640px',
+      'md': '768px',
+      '2md': '991px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1720px',
+    },
+    margin: {
+      'auto': 'auto',
+      '4px': '4px',
+      '6px': '6px',
+      '7px': '7px',
+      '8px': '8px',
+      '9px': '9px',
+      '10px': '10px',
+      '15px': '15px',
+      '20px': '20px',
+      '40px': '40px',
+      '41px': '41px',
+      '46px': '46px',
+      '50px': '50px',
+      '52px': '52px',
+      '76px': '76px',
+    },
+    padding: {
+      '9px': '9px',
+      '10px': '10px',
+      '14px': '14px',
+      '15px': '15px',
+      '16px': '16px',
+      '18px': '18px',
+      '20px': '20px',
+      '27px': '27px',
+      '30px': '30px',
+      '34px': '34px',
+      '48px': '48px',
+      '120px': '120px'
+    },
+    borderRadius: {
+      'none': '0',
+      'small': '4px',
+      'medium': '8px',
+      'large': '36px',
+    },
+    width: {
+      '16px': '16px',
+      '36px': '36px',
+      '256px': '256px',
+      '390px': '390px',
+      '610px': '610px',
+      '60%': '60%',
+      '100%': '100%'
+    },
+    height: {
+      '1.6px': '1.6px',
+      '34px': '34px',
+      '36px': '36px',
+      '50px': '50px'
+    },
+    minWidth: {
+      '30px': '30px',
+    },
+    inset: {
+      '9px': '9px',
+      '50%': '50%'
+    },
+    boxShadow: {
+      'shadow1': '0px 4px 4px rgba(217, 7, 9, 0.23)',
+      'shadow2': '0px 3px 7px rgba(217, 7, 9, 0.2)',
+      'shadow3': '0px 2px 10px rgba(0, 0, 0, 0.02)',
+      'shadow4': '0px 4px 30px rgba(0, 0, 0, 0.1)'
+    }
+  },
+  plugins: [
+   
+  ],
+}
